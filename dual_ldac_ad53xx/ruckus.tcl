dfe8d95e8d3d28883bc5a0bed582322784f3ccc2
Gregg Thayer <jgt@slac.stanford.edu>
2025-07-21 13:52:26 -0700

    dual_ldac_ad53xx

--- dual_ldac_ad53xx/ruckus.tcl
+++ dual_ldac_ad53xx/ruckus.tcl
@@ -3,6 +3,3 @@ source $::env(RUCKUS_PROC_TCL)
 
 # Load Source Code
 loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/dual_ldac_ad53xx_DAC_top.vhd"
-
-# Load Simulation
-#loadSource -lib lsst_reb -sim_only -dir "$::DIR_PATH/TB"
