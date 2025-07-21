96f2d9778e2058ded95fd9a55b2deef6b16a3fbd
Gregg Thayer <jgt@slac.stanford.edu>
2025-07-21 13:37:33 -0700

    dual_ad53xx

--- dual_ad53xx/rtl/demux_1_2_clk_def_1.vhd
+++ dual_ad53xx/rtl/demux_1_2_clk_def_1.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:03:07 07/05/2011 
--- Design Name: 
--- Module Name:    demux_1_2_clk_def_1 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    15:03:07 07/05/2011
+-- Design Name:
+-- Module Name:    demux_1_2_clk_def_1 - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
 library IEEE;
@@ -22,19 +22,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity demux_1_2_clk_def_1 is
 
 port (
     reset     : in  std_logic;           -- syncronus reset
     clk       : in  std_logic;           -- clock
-	 data_in  : in  std_logic;	 										-- data in
-	 selector : in  std_logic;
-	 data_out : out std_logic_vector(1 downto 0));          -- data out
+   data_in  : in  std_logic;                      -- data in
+   selector : in  std_logic;
+   data_out : out std_logic_vector(1 downto 0));          -- data out
 
 end demux_1_2_clk_def_1;
 
@@ -47,21 +42,21 @@ begin
 
 
 if clk'event and clk = '1' then  -- rising clock edge
-    if reset = '1' then                  -- synchronous reset 
+    if reset = '1' then                  -- synchronous reset
       data_out <= (others => '0');
     else
 
-	 ----------- default values -----------
+   ----------- default values -----------
       data_out <= (others => '1');
 
 
    case selector is
-      when '0' => data_out(0) 	<= data_in;
-      when '1' => data_out(1) 	<= data_in;
-		
+      when '0' => data_out(0)   <= data_in;
+      when '1' => data_out(1)   <= data_in;
+
       when others => data_out <= (others => '0');
    end case;
-	end if;
+  end if;
 end if;
 end process;
 
