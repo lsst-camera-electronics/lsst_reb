a4c09d1385f8a71e0f9420437d30fa9af3fdf21a
Gregg Thayer <jgt@slac.stanford.edu>
2025-07-21 13:24:49 -0700

    dcm_user_clk should be replaced with SURF clock manager

--- dcm_user_clk/ruckus.tcl
+++ dcm_user_clk/ruckus.tcl
@@ -2,9 +2,4 @@
 source $::env(RUCKUS_PROC_TCL)
 
 # Load Source Code
-# loadSource -lib lsst_reb -dir "$::DIR_PATH/rtl"
 loadIpCore -path "$::DIR_PATH/ip/dcm_user_clk.xci"
-# loadIpCore -dir "$::DIR_PATH/."
-
-# Load Simulation
-#loadSource -lib lsst_reb -sim_only -dir "$::DIR_PATH/TB"
