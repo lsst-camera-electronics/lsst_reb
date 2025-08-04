2760deb1831cac9cac944c799170c222429c8e3b
Gregg Thayer <jgt@slac.stanford.edu>
2025-08-04 16:01:30 -0700

    clk_2MHz_gen

--- clk_2MHz_gen/ruckus.tcl
+++ clk_2MHz_gen/ruckus.tcl
@@ -3,6 +3,3 @@ source $::env(RUCKUS_PROC_TCL)
 
 # Load Source Code
 loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/clk_2MHz_generator.vhd"
-
-# Load Simulation
-#loadSource -lib lsst_reb -sim_only -dir "$::DIR_PATH/TB"
