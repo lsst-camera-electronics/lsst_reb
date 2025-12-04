4f129b8ee0e3c55cbb038b16e213a9b90abc327a
Gregg Thayer <jgt@slac.stanford.edu>
2025-12-03 16:11:12 -0800

    Re-adjusted ADC handler SCK half period

--- ADC_data_handler_v4/rtl/ADC_data_handler_v4.vhd
+++ ADC_data_handler_v4/rtl/ADC_data_handler_v4.vhd
@@ -61,14 +61,14 @@ architecture Behavioral of ADC_data_handler_v4 is
   -- for 6.4ns clock: 1497.6ns=234, 1504ns=235
   ------------------------------------------------------------------------------
   constant DEV_CONV_TIME_C     : real :=  750.0E-9;
-  constant DEV_SCLK_HALF_PER_C : real :=   11.0E-9;
+  constant DEV_SCLK_HALF_PER_C : real :=   15.0E-9;
   constant DEV_TEST_TIME_C     : real := 1500.0E-9;
 
   constant CONV_TIME_C        : natural := calcClockPeriods(DEV_CONV_TIME_C,     CLK_PERIOD_G);
   constant SCLK_HALF_PERIOD_C : natural := calcClockPeriods(DEV_SCLK_HALF_PER_C, CLK_PERIOD_G);
   constant TEST_TIME_C        : natural := calcClockPeriods(DEV_TEST_TIME_C,     CLK_PERIOD_G);
   constant COL_INCR_VAL_C     : natural := 10;
-  constant PIX_INCR_CAL_C     : natural := 8;
+  constant PIX_INCR_CAL_C     : natural :=  8;
 
   signal cnt_en   : std_logic;
   signal init_cnt : std_logic;
