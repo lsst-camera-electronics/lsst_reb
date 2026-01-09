8145c31489643da4bb0f75d0c9fc565807352665
Gregg Thayer <jgt@slac.stanford.edu>
2026-01-09 10:53:15 -0800

    Updated sync_cmd_decoder while debugging spurious opcodes

--- sync_cmd_decoder/rtl/sync_cmd_decoder_top.vhd
+++ sync_cmd_decoder/rtl/sync_cmd_decoder_top.vhd
@@ -1,8 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-library unisim;
-use unisim.vcomponents.all;
+library surf;
+use surf.StdRtlPkg.all;
 
 library lsst_reb;
 
@@ -17,130 +17,95 @@ entity sync_cmd_decoder_top is
     delay_in           : in    std_logic_vector(7 downto 0);
     delay_read         : out   std_logic_vector(7 downto 0);
     sync_cmd           : in    std_logic_vector(7 downto 0);
-    sync_cmd_start_seq : out   std_logic; -- this signal is delayed buy at least
-    -- 1 clk with respect to sync_cmd_main_add
-    sync_cmd_step_seq : out   std_logic; -- this signal is delayed buy at least
-    -- 1 clk with respect to sync_cmd_main_add
-    sync_cmd_stop_seq : out   std_logic; -- this signal is delayed buy at least
-    -- 1 clk with respect to sync_cmd_main_add
-    sync_cmd_main_add : out   std_logic_vector(4 downto 0)
+    sync_cmd_start_seq : out   std_logic;
+    sync_cmd_step_seq  : out   std_logic;
+    sync_cmd_stop_seq  : out   std_logic;
+    sync_cmd_main_add  : out   std_logic_vector(4 downto 0)
   );
 end entity sync_cmd_decoder_top;
 
 architecture Behavioral of sync_cmd_decoder_top is
 
-  signal sync_cmd_en_stretch : std_logic;
-  signal sync_cmd_en_sync1   : std_logic;
-  signal sync_cmd_en_sync2   : std_logic;
-  signal sync_cmd_en_sync    : std_logic;
-  signal sync_cmd_latch      : std_logic_vector(7 downto 0);
   signal sync_cmd_start_int  : std_logic;
   signal sync_cmd_step_int   : std_logic;
   signal sync_cmd_stop_int   : std_logic;
   signal sync_cmd_addr_int   : std_logic_vector(4 downto 0);
   signal delay_in_reg        : std_logic_vector(7 downto 0);
+  signal delay_bus_in        : std_logic_vector(8 downto 0);
+  signal delay_bus_out       : std_logic_vector(8 downto 0);
+  signal sync_cmd_delay      : std_logic_vector(7 downto 0);
+  signal sync_cmd_en_delay   : std_logic;
+
+  signal sync_cmd_fifo_out    : std_logic_vector(7 downto 0);
+  signal sync_cmd_fifo_valid  : std_logic;
 
 begin
 
-  sync_cmd_reg : entity lsst_reb.generic_reg_ce_init
+  -- synchonize the input
+  sync_cmd_fifo : entity surf.SynchronizerFifo
     generic map (
-
-      width => 7
+      DATA_WIDTH_G => 8,
+      ADDR_WIDTH_G => 4
     )
     port map (
-      reset    => pgp_reset,
-      clk      => pgp_clk,
-      ce       => sync_cmd_en,
-      init     => '0',
-      data_in  => sync_cmd,
-      data_out => sync_cmd_latch
+      rst    => pgp_reset,
+      wr_clk => pgp_clk,
+      wr_en  => sync_cmd_en,
+      din    => sync_cmd,
+      rd_clk => clk,
+      dout   => sync_cmd_fifo_out,
+      valid  => sync_cmd_fifo_valid
     );
 
-  -- pulse stretcher
-  pulse_stretcher_A : entity lsst_reb.pulse_stretcher
+  -- Delay the command
+  delay_register : entity surf.RegisterVector
     generic map (
-      STRETCH_SIZE => 2
+      WIDTH_G => 8
     )
     port map (
-      clk         => pgp_clk,
-      reset       => pgp_reset,
-      sig_in      => sync_cmd_en,
-      stretch_out => sync_cmd_en_stretch
+      clk   => clk,
+      rst   => reset,
+      en    => delay_en,
+      sig_i => delay_in,
+      reg_o => delay_in_reg
     );
 
-  ff1_en : component FDRE
+  delay_bus_in <= sync_cmd_fifo_valid & sync_cmd_fifo_out;
+
+  sync_cmd_delay_reg : entity surf.SlvDelay
+    generic map (
+      DELAY_G => 255,
+      WIDTH_G => 9
+    )
     port map (
-      CE => '1',
-      R  => '0',
-      D  => sync_cmd_en_stretch,
-      C  => clk,
-      Q  => sync_cmd_en_sync1
+      clk   => clk,
+      rst   => reset,
+      delay => delay_in_reg,
+      din   => delay_bus_in,
+      dout  => delay_bus_out
     );
 
-  ff2_en : component FDRE
-    port map (
-      CE => '1',
-      R  => '0',
-      D  => sync_cmd_en_sync1,
-      C  => clk,
-      Q  => sync_cmd_en_sync
-    );
+  sync_cmd_en_delay <= delay_bus_out(8);
+  sync_cmd_delay    <= delay_bus_out(7 downto 0);
 
   sync_cmd_decoder_1 : entity lsst_reb.sync_cmd_decoder
     port map (
       clk            => clk,
       reset          => reset,
-      sync_cmd_en    => sync_cmd_en_sync,
-      sync_cmd       => sync_cmd_latch,
+      sync_cmd_en    => sync_cmd_en_delay,
+      sync_cmd       => sync_cmd_delay,
       sync_cmd_start => sync_cmd_start_int,
       sync_cmd_step  => sync_cmd_step_int,
       sync_cmd_stop  => sync_cmd_stop_int,
       sync_cmd_addr  => sync_cmd_addr_int
     );
 
-  delay_register : entity lsst_reb.generic_reg_ce_init
-    generic map (
 
-      width => 7
-    )
-    port map (
-      reset    => reset,
-      clk      => clk,
-      ce       => delay_en,
-      init     => '0',
-      data_in  => delay_in,
-      data_out => delay_in_reg
-    );
-
-  programmable_delay_start : entity lsst_reb.programmable_delay
-    port map (
-      clk        => clk,
-      reset      => reset,
-      signal_in  => sync_cmd_start_int,
-      delay_in   => delay_in_reg,
-      signal_out => sync_cmd_start_seq
-    );
-
-  programmable_delay_step : entity lsst_reb.programmable_delay
-    port map (
-      clk        => clk,
-      reset      => reset,
-      signal_in  => sync_cmd_step_int,
-      delay_in   => delay_in_reg,
-      signal_out => sync_cmd_step_seq
-    );
-
-  programmable_delay_stop : entity lsst_reb.programmable_delay
-    port map (
-      clk        => clk,
-      reset      => reset,
-      signal_in  => sync_cmd_stop_int,
-      delay_in   => delay_in_reg,
-      signal_out => sync_cmd_stop_seq
-    );
-
-  delay_read        <= delay_in_reg;
-  sync_cmd_main_add <= sync_cmd_addr_int;
+  delay_read         <= delay_in_reg;
+  sync_cmd_main_add  <= sync_cmd_addr_int;
+  sync_cmd_start_seq <= sync_cmd_start_int;
+  sync_cmd_step_seq  <= sync_cmd_step_int;
+  sync_cmd_stop_seq  <= sync_cmd_stop_int;
 
 end architecture Behavioral;
 
--- sync_cmd_decoder/ruckus.tcl
+++ sync_cmd_decoder/ruckus.tcl
@@ -4,5 +4,3 @@ source $::env(RUCKUS_PROC_TCL)
 # Load Source Code
 loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/sync_cmd_decoder_top.vhd"
 loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/sync_cmd_decoder.vhd"
-loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/pulse_stretcher.vhd"
-loadSource -lib lsst_reb -path "$::DIR_PATH/rtl/programmable_delay.vhd"
